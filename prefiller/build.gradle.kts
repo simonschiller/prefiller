00a2e1bb367777c939948ebc6f77ba3637cfd0ef
Simon Schiller <simon.schiller1998@gmail.com>
2021-01-25 16:19:52 +0100

    Bump version

--- prefiller/build.gradle.kts
+++ prefiller/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "io.github.simonschiller"
-version = "1.0.0" // Also update the version in the README
+version = "1.0.1" // Also update the version in the README
 
 repositories {
     google()
