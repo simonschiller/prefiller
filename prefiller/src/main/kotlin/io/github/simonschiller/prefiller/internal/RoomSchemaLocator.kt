e2a4475b26a042ca1b319dee62448086a09bf716
Simon Schiller <simon.schiller1998@gmail.com>
2021-05-05 13:28:15 +0200

    Bump Gradle to 7.0

--- prefiller/src/main/kotlin/io/github/simonschiller/prefiller/internal/RoomSchemaLocator.kt
+++ prefiller/src/main/kotlin/io/github/simonschiller/prefiller/internal/RoomSchemaLocator.kt
@@ -8,7 +8,7 @@ internal class RoomSchemaLocator {
         return schemaDirectory
             .listFiles { file -> file.extension.equals("json", ignoreCase = true) } // Find all JSON files
             ?.filter { file -> file.nameWithoutExtension.toIntOrNull() != null } // Ignore non-numeric file names
-            ?.maxBy { file -> file.nameWithoutExtension.toInt() } // Take the file with the highest database version
+            ?.maxByOrNull { file -> file.nameWithoutExtension.toInt() } // Take the file with the highest database version
             ?: error("Cannot find Room schema definition in $schemaDirectory, make sure Room is configured correctly")
     }
 }
