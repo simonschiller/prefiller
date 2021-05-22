e557b1537019c8ba7f623c548d7702e448321ccf
Simon Schiller <simon.schiller1998@gmail.com>
2021-05-22 16:48:34 +0200

    Remove duplicate sample sources

--- sample/kotlin-kotlin-dsl/src/main/sql/customers.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO customers(name, age) VALUES ("Mikael Burke", 38);
-INSERT INTO customers(name, age) VALUES ("Ayana Clarke", 12);
-INSERT INTO customers(name, age) VALUES ("Malachy Wall", 24);
-
-INSERT INTO creditcards(customerId, cardNumber) VALUES (1, "4098 6178 7375 6825");
-INSERT INTO creditcards(customerId, cardNumber) VALUES (3, "4003 8097 1909 0394");
