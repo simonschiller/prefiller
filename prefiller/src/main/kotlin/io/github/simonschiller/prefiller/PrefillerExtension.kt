65b73835842b385a8deed8cb7eb1ba6050220280
Simon Schiller <simon.schiller1998@gmail.com>
2021-06-15 11:32:26 +0200

    Unify Gradle and Kotlin DSL handling

--- prefiller/src/main/kotlin/io/github/simonschiller/prefiller/PrefillerExtension.kt
+++ prefiller/src/main/kotlin/io/github/simonschiller/prefiller/PrefillerExtension.kt
@@ -1,22 +1,14 @@
 package io.github.simonschiller.prefiller
 
-import groovy.lang.Closure
+import org.gradle.api.Action
 import org.gradle.api.model.ObjectFactory
-import org.gradle.util.ConfigureUtil
 
 open class PrefillerExtension(private val objects: ObjectFactory) {
     internal val databaseConfigs = mutableListOf<DatabaseConfig>()
 
-    // For configuration from Groovy DSL build files
-    fun database(name: String, config: Closure<Unit>) {
+    fun database(name: String, config: Action<DatabaseConfig>) {
         val databaseConfig = DatabaseConfig(name, objects)
-        ConfigureUtil.configure(config, databaseConfig)
-        addConfig(databaseConfig)
-    }
-
-    // For configuration from Kotlin DSL build files
-    fun database(name: String, config: DatabaseConfig.() -> Unit) {
-        val databaseConfig = DatabaseConfig(name, objects).apply(config)
+        config.execute(databaseConfig)
         addConfig(databaseConfig)
     }
 
